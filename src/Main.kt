82c8b538a85c95310de42e3e52ddb636afc06dfd
Joe Davidson <jdavidson@students.mchenry.edu>
2024-03-01 10:41:29 -0600

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,20 @@
+/**
+ * Represents a Customer with basic information.
+ * @author Joseph Davidson
+ * @property name The name of the customer.
+ * @property phone The phone number of the customer.
+ * @property address The address of the customer.
+ * @property squareFootage The square footage of the property.
+ */
 open class Customer(val name: String, val phone: String, val address: String, val squareFootage: Int)
 
+/**
+ * Represents a Commercial Customer that extends.
+ * @property propertyName The name of the commercial property.
+ * @property multiProperty Indicates if the customer has multiple properties.
+ */
 class Commercial(name: String, phone: String, address: String, squareFootage: Int, val propertyName: String, val multiProperty: Boolean) : Customer(name, phone, address, squareFootage) {
+// Calculate the weekly charge for the commercial customer.
     fun calculateWeeklyCharges(): Double {
         val rate = 5.0
         var totalCharge = squareFootage / 1000 * rate
@@ -11,7 +25,12 @@ class Commercial(name: String, phone: String, address: String, squareFootage: In
     }
 }
 
+/**
+ * Represents a Residential Customer that extends.
+ * val senior will determine if there will be a discount.
+ */
 class Residential(name: String, phone: String, address: String, squareFootage: Int, val senior: Boolean) : Customer(name, phone, address, squareFootage) {
+// Calculate the weekly charge for the residential customer.
     fun calculateWeeklyCharges(): Double {
         val rate = 6.0
         var totalCharge = squareFootage / 1000 * rate
@@ -21,7 +40,9 @@ class Residential(name: String, phone: String, address: String, squareFootage: I
         return totalCharge
     }
 }
-
+/**
+ * Main class to interact with the user and customer information.
+ */
 fun main() {
     var choice: Int
     do {
@@ -33,6 +54,7 @@ fun main() {
         choice = readLine()!!.toInt()
 
         when (choice) {
+    // Command fields for residential
             1 -> {
                 print("Enter customer name: ")
                 val name = readLine()!!
@@ -50,6 +72,7 @@ fun main() {
                 println("Residential Customer Information: \"$name\", \"$phone\", \"$address\", \"$squareFootage\" sqft")
                 println("Weekly Charge: $$weeklyCharge")
             }
+         // Command fields for commercial
             2 -> {
                 print("Enter customer name: ")
                 val name = readLine()!!
@@ -63,12 +86,13 @@ fun main() {
                 val propertyName = readLine()!!
                 print("Does the customer have multiple properties (true/false): ")
                 val multiProperty = readLine()!!.toBoolean()
-
+        // Information will be displayed in quotes 
                 val commercialCustomer = Commercial(name, phone, address, squareFootage, propertyName, multiProperty)
                 val weeklyCharge = commercialCustomer.calculateWeeklyCharges()
                 println("Commercial Customer Information: \"$name\", \"$phone\", \"$address\", \"$squareFootage sqft\", \"$propertyName\"")
                 println("Weekly Charge: $weeklyCharge")
             }
         }
+// This will end the program
     } while (choice != 3)
 }
